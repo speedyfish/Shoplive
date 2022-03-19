250b9c7f342de0bb8cb554f9cf1acb510a523231
speedyfish <brianlimjj5@gmail.com>
2022-03-19 14:41:37 +0800

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -46,7 +46,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Mr Brian</title>
+        <title>Mr Brian Smartypants</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
