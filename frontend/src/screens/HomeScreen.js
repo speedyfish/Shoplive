716f6c60075d78704935388e462fb00eb81fabdd
speedyfish <brianlimjj5@gmail.com>
2022-03-19 14:33:50 +0800

    Mr Brian

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -46,7 +46,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Mr Jerdwut R</title>
+        <title>Mr Brian</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
