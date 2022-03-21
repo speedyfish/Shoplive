13d1e2ee7893fea80d426158c64aa868f6c92b00
abigailchua <abigailchua.2020@scis.smu.edu.sg>
2022-03-21 16:42:37 +0800

    change source

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -48,9 +48,9 @@ export default function SigninScreen() {
       </Helmet>
 
       
-      <h1 className="my-3 text-white">Sign In</h1>
+      <h1 className="my-3">Sign In</h1>
       <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3 text-white" controlId="email">
+        <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
           <Form.Control
             type="email"
@@ -58,7 +58,7 @@ export default function SigninScreen() {
             onChange={(e) => setEmail(e.target.value)}
           />
         </Form.Group>
-        <Form.Group className="mb-3 text-white" controlId="password">
+        <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
@@ -69,7 +69,7 @@ export default function SigninScreen() {
         <div className="mb-3">
           <Button type="submit">Sign In</Button>
         </div>
-        <div className="mb-3 text-white">
+        <div className="mb-3">
           New customer?{" "}
           <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
         </div>
