a9da01cc928a8a0a3a061bf7b07f2ca5f11a9a78
abigailchua <abigailchua.2020@scis.smu.edu.sg>
2022-03-21 17:13:27 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,7 +59,7 @@ function App() {
                   </Link>
                   
                   {userInfo ? (
-                    <Link className="nav-link" to="/signout">
+                    <Link className="nav-link" to="/signin">
                     <img src="https://i.postimg.cc/76KGQfyg/User-02.png" alt="signin" height= "40" width="40"></img>
                     <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
                       <LinkContainer to="/profile">
